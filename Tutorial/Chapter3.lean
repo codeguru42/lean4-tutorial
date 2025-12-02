dc3fc01f45af5d96dc79f863034d49badb5d21a5
Code-Apprentice <codeguru42@gmail.com>
2025-12-02 04:54:07 -0700

    Add another section for more exercises

--- Tutorial/Chapter3.lean
+++ Tutorial/Chapter3.lean
@@ -105,3 +105,17 @@ section
   example : (p → q) → (¬q → ¬p) :=
     fun hpq : p → q => fun hnq : ¬q => fun hp : p => hnq (hpq hp)
 end
+
+section
+  open Classical
+
+  variable (p q r : Prop)
+
+  example : (p → q ∨ r) → ((p → q) ∨ (p → r)) := sorry
+  example : ¬(p ∧ q) → ¬p ∨ ¬q := sorry
+  example : ¬(p → q) → p ∧ ¬q := sorry
+  example : (p → q) → (¬p ∨ q) := sorry
+  example : (¬q → ¬p) → (p → q) := sorry
+  example : p ∨ ¬p := sorry
+  example : (((p → q) → p) → p) := sorry
+end
