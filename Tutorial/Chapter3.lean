9478264fec475e01625bcf10f560e65963f85cea
codeguru42 <codeguru42@gmail.com>
2025-12-03 07:05:07 -0700

    Formatting

--- Tutorial/Chapter3.lean
+++ Tutorial/Chapter3.lean
@@ -8,12 +8,14 @@ section
       (fun h : q ∧ p => And.intro h.right h.left)
   example : p ∨ q ↔ q ∨ p :=
     Iff.intro
-      (fun h: p ∨ q => (h.elim
-        (fun hp : p => Or.inr hp)
-        (fun hq : q => Or.inl hq)))
-      (fun h: q ∨ p => (h.elim
-          (fun hq : q => Or.inr hq)
-          (fun hp : p => Or.inl hp)))
+      (fun h: p ∨ q =>
+        (h.elim
+          (fun hp : p => Or.inr hp)
+          (fun hq : q => Or.inl hq)))
+      (fun h: q ∨ p =>
+          (h.elim
+            (fun hq : q => Or.inr hq)
+            (fun hp : p => Or.inl hp)))
 
   -- associativity of ∧ and ∨
   example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) :=
@@ -22,36 +24,44 @@ section
       (fun h : p ∧ (q ∧ r) => And.intro (And.intro h.left h.right.left) h.right.right)
   example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) :=
     Iff.intro
-      (fun h : (p ∨ q) ∨ r => h.elim
-        (fun hpq : p ∨ q => hpq.elim
-          (fun hp : p => Or.inl hp)
-          (fun hq : q => Or.inr (Or.inl hq)))
-        (fun hr : r => (Or.inr (Or.inr hr))))
-      (fun h : p ∨ (q ∨ r) => (h.elim
-        (fun hp : p => Or.inl (Or.inl hp))
-        (fun hqr : q ∨ r => hqr.elim
-          (fun hq : q => Or.inl (Or.inr hq))
-          (fun hr : r => Or.inr hr))))
+      (fun h : (p ∨ q) ∨ r =>
+        h.elim
+          (fun hpq : p ∨ q =>
+            hpq.elim
+              (fun hp : p => Or.inl hp)
+              (fun hq : q => Or.inr (Or.inl hq)))
+          (fun hr : r => (Or.inr (Or.inr hr))))
+      (fun h : p ∨ (q ∨ r) =>
+        (h.elim
+          (fun hp : p => Or.inl (Or.inl hp))
+          (fun hqr : q ∨ r => hqr.elim
+            (fun hq : q => Or.inl (Or.inr hq))
+            (fun hr : r => Or.inr hr))))
 
   -- distributivity
   example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
     Iff.intro
-      (fun h : p ∧ (q ∨ r) => h.right.elim
-        (fun hq : q => Or.inl (And.intro h.left hq))
-        (fun hr : r => Or.inr (And.intro h.left hr)))
-      (fun h : (p ∧ q) ∨ (p ∧ r) => h.elim
-      (fun hpq : (p ∧ q) => And.intro hpq.left (Or.inl hpq.right))
-      (fun hpr : (p ∧ r) => And.intro hpr.left (Or.inr hpr.right)))
+      (fun h : p ∧ (q ∨ r) =>
+        h.right.elim
+          (fun hq : q => Or.inl (And.intro h.left hq))
+          (fun hr : r => Or.inr (And.intro h.left hr)))
+      (fun h : (p ∧ q) ∨ (p ∧ r) =>
+        h.elim
+          (fun hpq : (p ∧ q) => And.intro hpq.left (Or.inl hpq.right))
+          (fun hpr : (p ∧ r) => And.intro hpr.left (Or.inr hpr.right)))
   example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) :=
     Iff.intro
-      (fun h : p ∨ (q ∧ r) => h.elim
-      (fun hp : p => And.intro (Or.inl hp) (Or.inl hp))
-      (fun hqr : q ∧ r => And.intro (Or.inr hqr.left) (Or.inr hqr.right)))
-      (fun h : (p ∨ q) ∧ (p ∨ r) => h.left.elim
-      (fun hp : p => Or.inl hp)
-      (fun hq : q => h.right.elim
-        (fun hp : p => Or.inl hp)
-        (fun hr : r => Or.inr (And.intro hq hr))))
+      (fun h : p ∨ (q ∧ r) =>
+        h.elim
+          (fun hp : p => And.intro (Or.inl hp) (Or.inl hp))
+          (fun hqr : q ∧ r => And.intro (Or.inr hqr.left) (Or.inr hqr.right)))
+      (fun h : (p ∨ q) ∧ (p ∨ r) =>
+        h.left.elim
+          (fun hp : p => Or.inl hp)
+          (fun hq : q =>
+            h.right.elim
+              (fun hp : p => Or.inl hp)
+              (fun hr : r => Or.inr (And.intro hq hr))))
 
   -- other properties
   example : (p → (q → r)) ↔ (p ∧ q → r) :=
@@ -60,26 +70,29 @@ section
       (fun h: p ∧ q → r => fun hp : p => fun hq : q => h (And.intro hp hq))
   example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) :=
     Iff.intro
-      (fun h : (p ∨ q) → r => And.intro
-        (fun hp : p => h (Or.inl hp))
-        (fun hq : q => h (Or.inr hq)))
+      (fun h : (p ∨ q) → r =>
+        And.intro
+          (fun hp : p => h (Or.inl hp))
+          (fun hq : q => h (Or.inr hq)))
       (fun h : (p → r) ∧ (q → r) =>
-      fun hpq : p ∨ q =>
-      hpq.elim (fun hp : p => h.left hp) (fun hq : q => h.right hq))
+       fun hpq : p ∨ q =>
+       hpq.elim (fun hp : p => h.left hp) (fun hq : q => h.right hq))
   example : ¬(p ∨ q) ↔ ¬p ∧ ¬q :=
     Iff.intro
-      (fun h : ¬(p ∨ q) => And.intro
-        (fun hp : p => h (Or.inl hp))
-        (fun hq : q => h (Or.inr hq)))
+      (fun h : ¬(p ∨ q) =>
+        And.intro
+          (fun hp : p => h (Or.inl hp))
+          (fun hq : q => h (Or.inr hq)))
       (fun h : ¬p ∧ ¬q =>
-      fun hpq : p ∨ q => hpq.elim
-        (fun hp : p => h.left hp)
-        (fun hq : q => h.right hq))
+       fun hpq : p ∨ q => hpq.elim
+         (fun hp : p => h.left hp)
+         (fun hq : q => h.right hq))
   example : ¬p ∨ ¬q → ¬(p ∧ q) :=
     fun hnpnq : ¬p ∨ ¬q =>
-    fun hpq : p ∧ q => hnpnq.elim
-      (fun hnp : ¬p => hnp hpq.left)
-      (fun hnq : ¬q => hnq hpq.right)
+    fun hpq : p ∧ q =>
+      hnpnq.elim
+        (fun hnp : ¬p => hnp hpq.left)
+        (fun hnq : ¬q => hnq hpq.right)
   example : ¬(p ∧ ¬p) :=
     fun hpnp : p ∧ ¬p => hpnp.right hpnp.left
   example : p ∧ ¬q → ¬(p → q) :=
@@ -89,14 +102,16 @@ section
     fun hnp : ¬p => fun hp : p => absurd hp hnp
   example : (¬p ∨ q) → (p → q) :=
     fun hnpq : ¬p ∨ q =>
-    fun hp : p => hnpq.elim
-      (fun hnp : ¬p => absurd hp hnp)
-      (fun hq : q => hq)
+    fun hp : p =>
+      hnpq.elim
+        (fun hnp : ¬p => absurd hp hnp)
+        (fun hq : q => hq)
   example : p ∨ False ↔ p :=
     Iff.intro
-      (fun hpf : p ∨ False => hpf.elim
-        (fun hp : p => hp)
-        (fun hf : False => hf.elim))
+      (fun hpf : p ∨ False =>
+        hpf.elim
+          (fun hp : p => hp)
+          (fun hf : False => hf.elim))
       (fun hp : p => Or.inl hp)
   example : p ∧ False ↔ False :=
     Iff.intro
