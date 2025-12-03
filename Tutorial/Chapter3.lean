61c1e73ab7466749231998d4d9de33bd0946d498
codeguru42 <codeguru42@gmail.com>
2025-12-03 07:00:36 -0700

    Use `absurd`

--- Tutorial/Chapter3.lean
+++ Tutorial/Chapter3.lean
@@ -86,11 +86,11 @@ section
     fun hpnq : p ∧ ¬q =>
     fun hpq : p → q => hpnq.right (hpq hpnq.left)
   example : ¬p → (p → q) :=
-    fun hnp : ¬p => fun hp : p => (hnp hp).elim
+    fun hnp : ¬p => fun hp : p => absurd hp hnp
   example : (¬p ∨ q) → (p → q) :=
     fun hnpq : ¬p ∨ q =>
     fun hp : p => hnpq.elim
-      (fun hnp : ¬p => (hnp hp).elim)
+      (fun hnp : ¬p => absurd hp hnp)
       (fun hq : q => hq)
   example : p ∨ False ↔ p :=
     Iff.intro
