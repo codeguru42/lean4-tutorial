81851eb3d994cbf4ce6aa1004e3a7d2cb51da343
codeguru42 <codeguru42@gmail.com>
2025-12-09 19:32:07 -0700

    Prove α → ((∀ x : α, r) ↔ r)

--- Tutorial/Chapter4.lean
+++ Tutorial/Chapter4.lean
@@ -30,7 +30,11 @@ second of these requires classical logic):
 variable (α : Type) (p q : α → Prop)
 variable (r : Prop)
 
-example : α → ((∀ x : α, r) ↔ r) := sorry
+example : α → ((∀ x : α, r) ↔ r) :=
+  fun x =>
+    Iff.intro
+      (fun r => r x)
+      (fun r => fun _ => r)
 example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
 example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
 
