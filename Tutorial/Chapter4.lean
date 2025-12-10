5f5fed79fb6d96993419d7d6f7ac5b9780a161fc
codeguru42 <codeguru42@gmail.com>
2025-12-09 19:01:38 -0700

    Format instructions in comments

--- Tutorial/Chapter4.lean
+++ Tutorial/Chapter4.lean
@@ -8,8 +8,13 @@ example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
 example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
 
 /-
- 2. You should also try to understand why the reverse implication is not derivable in the last example.
-It is often possible to bring a component of a formula outside a universal quantifier, when it does not depend on the quantified variable. Try proving these (one direction of the second of these requires classical logic):
+ 2. You should also try to understand why the reverse
+ implication is not derivable in the last example.
+
+It is often possible to bring a component of a formula outside
+a universal quantifier, when it does not depend on the
+quantified variable. Try proving these (one direction of the
+second of these requires classical logic):
  -/
 variable (α : Type) (p q : α → Prop)
 variable (r : Prop)
@@ -19,7 +24,10 @@ example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
 example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
 
 /-
-Consider the “barber paradox,” that is, the claim that in a certain town there is a (male) barber that shaves all and only the men who do not shave themselves. Prove that this is a contradiction:
+3. Consider the “barber paradox,” that is, the claim that in a
+certain town there is a (male) barber that shaves all and
+only the men who do not shave themselves. Prove that this is
+a contradiction:
  -/
 variable (men : Type) (barber : men)
 variable (shaves : men → men → Prop)
@@ -28,7 +36,15 @@ example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False :=
   sorry
 
 /-
-Remember that, without any parameters, an expression of type Prop is just an assertion. Fill in the definitions of prime and Fermat_prime below, and construct each of the given assertions. For example, you can say that there are infinitely many primes by asserting that for every natural number n, there is a prime number greater than n. Goldbach's weak conjecture states that every odd number greater than 5 is the sum of three primes. Look up the definition of a Fermat prime or any of the other statements, if necessary.
+4. Remember that, without any parameters, an expression of type
+Prop is just an assertion. Fill in the definitions of prime
+and Fermat_prime below, and construct each of the given
+assertions. For example, you can say that there are infinitely
+many primes by asserting that for every natural number n,
+there is a prime number greater than n. Goldbach's weak
+conjecture states that every odd number greater than 5 is
+the sum of three primes. Look up the definition of a Fermat
+prime or any of the other statements, if necessary.
  -/
 def even (n : Nat) : Prop := sorry
 
@@ -47,5 +63,6 @@ def Goldbach's_weak_conjecture : Prop := sorry
 def Fermat's_last_theorem : Prop := sorry
 
 /-
- Prove as many of the identities listed in the Existential Quantifier section as you can.
+5.  Prove as many of the identities listed in the Existential
+ Quantifier section as you can.
  -/
