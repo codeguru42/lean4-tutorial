53f9bd70033334336b61f40a9a481aebb64bed27
codeguru42 <codeguru42@gmail.com>
2025-12-21 13:45:41 -0700

    Exercises for Chapter 5

---
+++ Tutorial/Chapter5.lean
@@ -0,0 +1,129 @@
+section
+  section
+    -- Prove the following identities, replacing the sorry placeholders with actual proofs.
+    variable (p q r : Prop)
+
+    -- commutativity of ∧ and ∨
+    example : p ∧ q ↔ q ∧ p := sorry
+    example : p ∨ q ↔ q ∨ p := sorry
+
+    -- associativity of ∧ and ∨
+    example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+    example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+
+    -- distributivity
+    example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+    example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+
+    -- other properties
+    example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+    example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+    example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+    example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+    example : ¬(p ∧ ¬p) := sorry
+    example : p ∧ ¬q → ¬(p → q) := sorry
+    example : ¬p → (p → q) := sorry
+    example : (¬p ∨ q) → (p → q) := sorry
+    example : p ∨ False ↔ p := sorry
+    example : p ∧ False ↔ False := sorry
+    example : (p → q) → (¬q → ¬p) := sorry
+  end
+
+  section
+    -- Prove the following identities, replacing the sorry placeholders with actual proofs. These require classical reasoning.
+
+    open Classical
+
+    variable (p q r : Prop)
+
+    example : (p → q ∨ r) → ((p → q) ∨ (p → r)) := sorry
+    example : ¬(p ∧ q) → ¬p ∨ ¬q := sorry
+    example : ¬(p → q) → p ∧ ¬q := sorry
+    example : (p → q) → (¬p ∨ q) := sorry
+    example : (¬q → ¬p) → (p → q) := sorry
+    example : p ∨ ¬p := sorry
+    example : (((p → q) → p) → p) := sorry
+
+  end
+
+  -- Prove ¬(p ↔ ¬p) without using classical logic.
+end
+
+section
+  section
+    -- 1. Prove these equivalences:
+    variable (α : Type) (p q : α → Prop)
+
+    example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
+    example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
+    example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
+  end
+
+  section
+    /-
+    2. It is often possible to bring a component of a formula outside
+    a universal quantifier, when it does not depend on the quantified
+    variable. Try proving these (one direction of the second of these
+    requires classical logic):
+    -/
+
+    variable (α : Type) (p q : α → Prop)
+    variable (r : Prop)
+
+    example : α → ((∀ x : α, r) ↔ r) := sorry
+    example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
+    example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
+  end
+
+  section
+    /-
+    3. Consider the “barber paradox,” that is, the claim that in a
+    certain town there is a (male) barber that shaves all and
+    only the men who do not shave themselves. Prove that this is
+    a contradiction:
+    -/
+
+    variable (men : Type) (barber : men)
+    variable (shaves : men → men → Prop)
+
+    example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False :=
+      sorry
+  end
+
+  section
+    /-
+    4. Remember that, without any parameters, an expression of type
+    Prop is just an assertion. Fill in the definitions of prime
+    and Fermat_prime below, and construct each of the given
+    assertions. For example, you can say that there are infinitely
+    many primes by asserting that for every natural number n,
+    there is a prime number greater than n. Goldbach's weak
+    conjecture states that every odd number greater than 5 is
+    the sum of three primes. Look up the definition of a Fermat
+    prime or any of the other statements, if necessary.
+    -/
+
+    def even (n : Nat) : Prop := sorry
+
+    def prime (n : Nat) : Prop := sorry
+
+    def infinitely_many_primes : Prop := sorry
+
+    def Fermat_prime (n : Nat) : Prop := sorry
+
+    def infinitely_many_Fermat_primes : Prop := sorry
+
+    def goldbach_conjecture : Prop := sorry
+
+    def Goldbach's_weak_conjecture : Prop := sorry
+
+    def Fermat's_last_theorem : Prop := sorry
+  end
+
+  section
+    /-
+    5.  Prove as many of the identities listed in the Existential
+    Quantifier section as you can.
+    -/
+  end
+end
