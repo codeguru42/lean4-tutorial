69b104b659fb239c84e9ce8c7e60466261f0c47b
Code-Apprentice <codeguru42@gmail.com>
2025-12-25 16:10:59 -0700

    Exerciess with existential quantifier

--- Tutorial/Chapter5.lean
+++ Tutorial/Chapter5.lean
@@ -340,5 +340,24 @@ section
     5.  Prove as many of the identities listed in the Existential
     Quantifier section as you can.
     -/
+
+    open Classical
+
+    variable (α : Type) (p q : α → Prop)
+    variable (r : Prop)
+
+    example : (∃ x : α, r) → r := sorry
+    example (a : α) : r → (∃ x : α, r) := sorry
+    example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := sorry
+    example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := sorry
+
+    example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := sorry
+    example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := sorry
+    example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
+    example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
+
+    example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
+    example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := sorry
+    example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := sorry
   end
 end
