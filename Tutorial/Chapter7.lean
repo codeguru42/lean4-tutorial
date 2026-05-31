4387f58811bb628633eef687c67c087e4236e45c
codeguru42 <codeguru42@gmail.com>
2026-05-31 10:33:00 -0600

    Clean up some duplication

--- Tutorial/Chapter7.lean
+++ Tutorial/Chapter7.lean
@@ -40,20 +40,6 @@ theorem nil_append (as : List α) : append nil as = as :=
 theorem cons_append (a : α) (as bs : List α)
                     : append (cons a as) bs = cons a (append as bs) :=
  rfl
-namespace Hidden
-inductive List (α : Type u) where
-| nil  : List α
-| cons : α → List α → List α
-namespace List
-def append (as bs : List α) : List α :=
- match as with
- | nil       => bs
- | cons a as => cons a (append as bs)
-theorem nil_append (as : List α) : append nil as = as :=
- rfl
-theorem cons_append (a : α) (as bs : List α)
-                    : append (cons a as) bs = cons a (append as bs) :=
- rfl
 theorem append_nil (as : List α) :
     append as nil = as :=
   sorry
